2fec7f3c701f5bcbb7020efa61372c34e158c79c
aristeh <aristeh.otoko@gmail.com>
2020-02-17 16:45:13 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace GetStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new DataModels.FIRSTDB())
+
+            {
+                var q =
+                    from c in db.Departments
+                    select c;
+
+                foreach (var c in q)
+                    Console.WriteLine(c.ID);
+            }
+        }
+    }
+}
